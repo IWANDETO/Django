facb18675b08a17b5f13c84598631f0a2a5391cf
IWANDETO <wandettoian@gmail.com>
2024-06-05 20:20:42 +0300

    Added method shortcuts provided by Django

--- Docs/Poll/mysite/polls/views.py
+++ Docs/Poll/mysite/polls/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
-def index(reqeust):
+def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     template = loader.get_template("polls/index.html")
     context = {
@@ -10,8 +13,26 @@ def index(reqeust):
     }
     return HttpResponse(template.render(context, request))
 
+#A shortcut: render()    
+#It’s a very common idiom to load a template, fill a context and return an HttpResponse object with the result of the rendered template. Django provides a shortcut.
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(reuest, "polls/detail.html", {"question": question})
+
+# A shortcut: get_object_or_404()
+# It’s a very common idiom to use get() and raise Http404 if the object doesn’t exist. Django provides a shortcut. Here’s the detail() view, rewritten:
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
